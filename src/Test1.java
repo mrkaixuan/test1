7c2cb5ee9dfa8279652891f16528f248ea72e893
hkx <hkxmail@163.com>
2020-05-23 20:06:18 +0800

    Commit to github with 2

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,8 @@
+import org.omg.CosNaming._BindingIteratorImplBase;
+
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("my first git test");
+        System.out.println("test");
     }
 }
