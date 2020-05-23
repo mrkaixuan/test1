2802ce475cf3b20ea7891c9a4999c0023f2b551e
hkx <hkxmail@163.com>
2020-05-23 22:26:02 +0800

    Commit to github with 2

--- src/Test1.java
+++ src/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("my first git test");
         System.out.println("test");
+        System.out.println("test2");
     }
 }
